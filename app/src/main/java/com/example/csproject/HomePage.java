73ed99b1b71d6bbdfcd90e627125737b73e4f49e
user <guyamibar@gmail.com>
2023-10-24 11:56:11 +0300

    menu but not connected to database

--- app/src/main/java/com/example/csproject/HomePage.java
+++ app/src/main/java/com/example/csproject/HomePage.java
@@ -3,6 +3,7 @@ package com.example.csproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.core.view.WindowCompat;
@@ -12,29 +13,33 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener{
     private Toolbar toolbar;
     private View decorView;
     private DrawerLayout drawer;
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
+    private TextView profile_icon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        hideSystemUI(getWindow());
+        getWindow().getDecorView().setSystemUiVisibility(hideSystemBars());
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
         }
@@ -43,6 +48,7 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -55,10 +61,6 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_home);
         }
-
-        database = FirebaseDatabase.getInstance("https://csproject-99c38-default-rtdb.europe-west1.firebasedatabase.app/");
-        mAuth = FirebaseAuth.getInstance();
-
         decorView = getWindow().getDecorView();
         decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
             @Override
@@ -67,6 +69,11 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
                     decorView.setSystemUiVisibility(hideSystemBars());
             }
         });
+
+        database = FirebaseDatabase.getInstance("https://csproject-99c38-default-rtdb.europe-west1.firebasedatabase.app/");
+        mAuth = FirebaseAuth.getInstance();
+        profile_icon=findViewById(R.id.profile_icon);
+        profile_icon.setOnClickListener(this);
     }
 
     @Override
@@ -113,20 +120,22 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
             decorView.setSystemUiVisibility(hideSystemBars());
     }
 
-    public static void hideSystemUI(android.view.Window window) {
-        window.getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-    }
 
     public void signOut() {
         mAuth.signOut();
         Intent i = new Intent(HomePage.this, MainActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==profile_icon){
+            showPopup(view);
+        }
+    }
+    public void showPopup(View v){
+        PopupMenu popup = new PopupMenu(this,v);
+        popup.inflate(R.menu.profile_menu);
+        popup.show();
     }
 }
