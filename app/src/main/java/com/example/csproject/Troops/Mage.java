33d84e56a9dd915b5c27f952635136a65ed5601f
guyam <guyamibar@gmail.com>
2024-03-12 20:15:23 +0200

    buffs disapear after attack and there is a gamage dictionary

--- app/src/main/java/com/example/csproject/Troops/Mage.java
+++ app/src/main/java/com/example/csproject/Troops/Mage.java
@@ -11,9 +11,7 @@ public class Mage extends Troop
     public boolean buffTroop(Troop troop)//checks if the troop is already buffed, if it isnt, it buffs the troop and returns true
     {
         int[] troopPos = troop.getPosition();
-        int[] magePos = getPosition();
-        if(Math.abs(magePos[0]-troopPos[0])>getAttackRange()|| //if out of range
-           Math.abs(magePos[1]-troopPos[1])>getAttackRange()||
+        if(!getPositionsInAttackRange().contains(troopPos)||
            troop.getMaged())//or already buffed
             return false;
         troop.setDmg(troop.getDmg()+1);
